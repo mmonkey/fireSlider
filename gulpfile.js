445781ecf5068013b21f1aef495ce338812a978b
CJ O'Hara <cj.ohara@firespring.com>
2014-12-16 17:30:21 -0600

    added sass to gulp and updated watching for html and sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
 	jshint = require('gulp-jshint'),
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
+	notify = require('gulp-notify'),
 	browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+	return gulp.src(['build/scss/*.scss', '!build/scss/_*.scss'])
+		.pipe(sass({
+			style: 'compressed',
+			sourcemapPath: 'build/css'
+		}))
+		.on('error', function (err) { console.log(err.message); })
+		.pipe(gulp.dest('build/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
 
 gulp.task('lint', function() {
 	return gulp.src(['build/js/*.js', '!build/js/**/*.min.js'])
@@ -38,6 +51,8 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('default', ['browser-sync'], function() {
-	gulp.start('lint', 'beautify', 'compress');
-	gulp.watch('js/*.js', ['lint', 'beautify', 'compress', browserSync.reload]);
+	gulp.start('sass', 'lint', 'beautify', 'compress');
+	gulp.watch('build/js/*.js', ['lint', 'beautify', 'compress', browserSync.reload]);
+	gulp.watch('**/*.html', browserSync.reload);
+	gulp.watch('build/scss/**/*.scss', ['sass']);
 });
